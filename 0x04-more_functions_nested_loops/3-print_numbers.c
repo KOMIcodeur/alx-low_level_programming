2aa4be42d11eff8b5493538d3dbedb9bfba733eb
KOMIcodeur <adolphesade@gmail.com>
2023-03-24 13:24:50 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0-9
+ * Return: void
+ */
+
+void print-numbers(void)
+{
+        char c;
+
+        for (c = '0'; c <= '9'; c++)
+        {
+                _putchar(0);
+        }
+        _putchart('\n');
+}
