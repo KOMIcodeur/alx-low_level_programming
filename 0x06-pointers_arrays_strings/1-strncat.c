7ebf9985faa51d5308faa1dd4cd080ca2f2c9b76
KOMIcodeur <adolphesade@gmail.com>
2023-03-31 18:22:48 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * strncat - concatenates two string
+ * @dest: p1
+ * @ src : p2
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+	int srclen = 0;
+	int destlen = 0;
+	int i;
+
+	for (i = 0; src[i] != '\0' ; i++)
+		srclen++;
+	for (i = 0; dest[i] != '\0' ; i++)
+		destlen++;
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+		dest[destlen + i] = src[i];
+	dest[destlen + i] = '\0';
+
+	
+	return dest;
+}
